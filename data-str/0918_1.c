f70b30bd7ac93d777115d79eec882289bdbfc3b8
kuc4126 <rdmsco2006@gmail.com>
2025-09-18 11:32:23 +0900

    ㅇ

---
+++ data-str/0918_1.c
@@ -0,0 +1,28 @@
+//250918강의날짜
+//1-7 예재
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    putc("1부터 n까지의 총합을 구합니다.");
+
+    printf("n값: ");
+    scanf("%d", &n);
+
+    int sum = 0;
+    int i = 1;
+
+    while (i <= n)
+    {
+        sum += i;
+        i++;
+    }
+
+    printf("1부터 %d까지의 총합은 %d입니다.\n", n, sum);
+
+    return 0;
+    
+}
