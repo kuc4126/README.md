abef7d4979fdf2ae2e8733c016bf12127a7bdc75
kuc4126 <rdmsco2006@gmail.com>
2025-09-18 11:38:40 +0900

    d

--- data-str/0918_1.c
+++ data-str/0918_1.c
@@ -1,4 +1,4 @@
-//250918강의날짜
+//250918
 //1-7 예재
 
 #include <stdio.h>
@@ -7,13 +7,14 @@ int main(void)
 {
     int n;
 
-    putc("1부터 n까지의 총합을 구합니다.");
+    puts("1부터 n까지의 총합을 구합니다.");
 
     printf("n값: ");
     scanf("%d", &n);
 
     int sum = 0;
     int i = 1;
+    int sum2 = 0;
 
     while (i <= n)
     {
@@ -21,7 +22,13 @@ int main(void)
         i++;
     }
 
-    printf("1부터 %d까지의 총합은 %d입니다.\n", n, sum);
+    for (int j = 1; j <= n; j++)
+    {
+        sum2 += j;
+    }
+
+    printf("while-1부터 %d까지의 총합은 %d입니다.\n", n, sum);
+    printf("for-1부터 %d까지의 총합은 %d입니다.\n", n, sum2);
 
     return 0;
     
