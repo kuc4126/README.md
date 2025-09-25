ceedeccc2e9050428a347c41b4c5fdd357df2b5a
kuc4126 <rdmsco2006@gmail.com>
2025-09-25 10:35:36 +0900

    0925수업 시작

---
+++ data-str/0925.c
@@ -0,0 +1,20 @@
+//202530208 김은채 0925강의
+
+
+#include <stdio.h>
+
+#define N 5
+int main(){
+    int foo[N];
+    for (int i = 0; i < N; i++){
+        printf("foo[%d] : ", i);
+        scanf("%d", &foo[i]);
+    }
+    puts("각 요소의 값");
+    for (int i = 0; i < N; i++)
+    {
+        printf("foo[%d] = %d\n", i, foo[i]);
+    }
+    
+    return 0;
+}
