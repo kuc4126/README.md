b638bee2aa902872ebf226d54ff1db1d8ea7b85b
kuc4126 <rdmsco2006@gmail.com>
2025-10-30 11:06:01 +0900

    s

--- data-str/1030.c
+++ data-str/1030.c
@@ -31,5 +31,124 @@
 #   이 상태에서 y 함수의 실행이 종료되면 y 함수만 팝함
 
 
+# 스택 구조체 IntStack
 
+# 스택으로 사용할 배열을 가리키는 포인터 stk
+#   인덱스가 0인 요소를 스택의 바닥 bottom 이라 함
+#   배열의 메모리 공간 할당은 Initialize 함수로 생성
+
+# 스택의 최대 용량 max
+#   배열 stk의 요소 개수와 같음
+
+# 스택 포인터 ptr
+#   스택에 쌓여 있는 데이터의 개수를 나타냄
+#   스택이 비어 있으면 ptr의 값은 0이고 가득 차 있으면 max
 */
+// int형 스택 IntStack (헤더)
+
+
+/*#ifndef ___IntStack
+#define ___IntStack
+
+//스택을 구현하는 구조체
+typedef struct {
+    int max;   // 스택 용량
+    int ptr;   // 스택 포인터
+    int *stk;  // 스택의 첫 요소에 대한 포인터
+} IntStack;
+
+#endif*/
+
+
+// int형 큐 IntQueue (소스)
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include "IntQueue.h"
+
+// 큐 초기화
+int Initialize(IntQueue *q, int max)
+{
+    q->num = q->front = q->rear = 0;
+    if ((q->que = calloc(max, sizeof(int))) == NULL) {
+        q->max = 0;          // 배열 생성에 실패
+        return -1;
+    }
+    q->max = max;
+    return 0;
+}
+
+// 큐에 데이터를 인큐
+int Enque(IntQueue *q, int x)
+{
+    if (q->num >= q->max)     // 큐가 가득 참
+        return -1;
+    else {
+        q->num++;
+        q->que[q->rear++] = x;
+        if (q->rear == q->max)
+            q->rear = 0;
+        return 0;
+    }
+}
+
+// 큐에서 데이터를 디큐
+int Deque(IntQueue *q, int *x)
+{
+    if (q->num <= 0)          // 큐는 비어 있음
+        return -1;
+    else {
+        q->num--;
+        *x = q->que[q->front++];
+        if (q->front == q->max)
+            q->front = 0;
+        return 0;
+    }
+}
+*/
+
+// int형 IntStack의 사용
+#include <stdio.h>
+#include "IntStack.h"
+
+int main(void)
+{
+	IntStack s;
+	if (Initialize(&s, 64) == -1)
+	{
+		puts("스택 생성에 실패했습니다.");
+		return 1;
+	}
+
+	while (1)
+	{
+		int menu, x;
+		printf("현재 데이터 수: %d / %d\n", Size(&s), Capacity(&s));
+		printf("(1)푸시 (2)팝 (3)피크 (4)출력 (0)종료: ");
+		scanf("%d", &menu);
+
+		if (menu == 0)
+			break;
+
+		switch (menu)
+		{
+		 case 1: /*--- 푸시 ---*/
+			printf("데이터: ");
+			scanf("%d", &x);
+			if (Push(&s, x) == -1)
+				puts("\a오류: 푸시에 실패했습니다.");
+			break;
+
+		 case 2: /*--- 팝 ---*/
+			if (Pop(&s, &x) == -1)
+				puts("\a오류: 팝에 실패했습니다.");
+			else
+				printf("팝 데이터는 %d입니다.\n", x);
+			break;
+
+		 case 3: /*--- 피크 ---*/
+			if (Peek(&s, &x) == -1)
+				puts("\a오류: 피크에 실패했습니다.");
+			break;
+		}
+	}
